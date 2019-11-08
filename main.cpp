a3bd28e2a039e774743c3f41bc0467c8d269726f
Mauricio-Bernuy <54604331+Mauricio-Bernuy@users.noreply.github.com>
2019-11-07 19:01:55 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -28,23 +28,23 @@ int main() {
 				istringstream bid(line);
 				vector<string> temp{ istream_iterator<string>{bid},
 					  istream_iterator<string>{} };
-				
-				BidderBid.insert(pair<string, int>(temp[0],stoi(temp[1])));
-				cout << temp[0]<<" "<< temp[1]<<endl;
+
+				BidderBid.insert(pair<string, int>(temp[0], stoi(temp[1])));
+				cout << temp[0] << " " << temp[1] << endl;
 			}
 			else {
-				if (BidderBid.size() != 0) { //sort and write back to 
+				if (BidderBid.size() != 0) { //sort and write to output
 
 					//mayor valor
 					auto max = max_element(BidderBid.begin(), BidderBid.end(),
 						[](const pair<string, int>& p1, const pair<string, int>& p2) {
 							return p1.second < p2.second; });
-					
-					output << max->second<<",";
+
+					output << max->second << ",";
 					//promedio
-					output << (accumulate(BidderBid.begin(), BidderBid.end(), 0, [](int acc, std::pair<string, int> p) { return (acc + p.second); }))/BidderBid.size() << ",";
-					
-					//mayor menor
+					output << (accumulate(BidderBid.begin(), BidderBid.end(), 0, [](int acc, std::pair<string, int> p) { return (acc + p.second); })) / BidderBid.size() << ",";
+
+					//menor menor
 					auto min = min_element(BidderBid.begin(), BidderBid.end(),
 						[](const pair<string, int>& p1, const pair<string, int>& p2) {
 							return p1.second < p2.second; });
@@ -52,23 +52,61 @@ int main() {
 					output << min->second << endl;
 
 					//write
-					for (auto itr = BidderBid.begin(); itr != BidderBid.end(); ++itr) {
-						output << itr->first <<" "<< itr->second << '\n';
+					/*
+					vector<string, int> OrderedOutput;
+
+					copy(BidderBid.begin(), BidderBid.end(), back_inserter<vector<string, int>>(OrderedOutput));
+
+					sort(OrderedOutput.begin(), OrderedOutput.end(),
+						[](const pair<string, int>& p1, const pair<string, int>& p2) {
+							if (p1.second != p2.second)
+								return p1.second < p2.second;
+							return p1.first < p2.first;
+						});
+
+					for (auto itr = OrderedOutput.begin(); itr != OrderedOutput.end(); ++itr) {
+						output << itr[0] <<" "<< itr[1] << '\n';
 					}
-					cout << endl;	
+					*/
+					for (auto itr = BidderBid.begin(); itr != BidderBid.end(); ++itr) {
+						output << itr->first << " " << itr->second << '\n';
+					}
+					cout << endl;
 				}
 
 				cout << line << "(product name detected)" << endl;
 				currentuser = line;
-				
+
 				output << currentuser << ",";
 
 				BidderBid.clear();
 				currentuser.clear();
+
+			}
 			
+		}
+		if (BidderBid.size() != 0) { //sort and write to output
+
+					//mayor valor
+			auto max = max_element(BidderBid.begin(), BidderBid.end(),
+				[](const pair<string, int>& p1, const pair<string, int>& p2) {
+					return p1.second < p2.second; });
+
+			output << max->second << ",";
+			//promedio
+			output << (accumulate(BidderBid.begin(), BidderBid.end(), 0, [](int acc, std::pair<string, int> p) { return (acc + p.second); })) / BidderBid.size() << ",";
+
+			//menor menor
+			auto min = min_element(BidderBid.begin(), BidderBid.end(),
+				[](const pair<string, int>& p1, const pair<string, int>& p2) {
+					return p1.second < p2.second; });
+
+			output << min->second << endl;
+			//write
+			for (auto itr = BidderBid.begin(); itr != BidderBid.end(); ++itr) {
+				output << itr->first << " " << itr->second << '\n';
 			}
 		}
-
 		input.close();
 	}
 }
